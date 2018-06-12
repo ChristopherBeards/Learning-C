c8348fc1060a9ae413c9e76603806a71211960b8
Christopher Beards <beardscm@gmail.com>
2018-06-12 15:58:46 -0400

    Printing an integer; added

---
+++ C-Introduction/Integer-Output/integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int test = 1;
+  printf("Printing number %d", test); // %d -> for integer
+  return 0;
+}
+
+//-------------------------------
+//            OUTPUT
+//-------------------------------
+
+// -> Printing number 1
