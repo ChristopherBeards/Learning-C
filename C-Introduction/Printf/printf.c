95aaf02c850edbd42e7ee0af1f610eb8f107d7b8
Christopher Beards <beardscm@gmail.com>
2018-06-12 15:50:39 -0400

    Printf; Added

---
+++ C-Introduction/Printf/printf.c
@@ -0,0 +1,14 @@
+// The scanf() function reads formatted input from standard input 
+
+#include <stdio.h> // Contains the printf() function
+
+int main() {
+  printf("Learning C"); // displays the data within quotes
+  return 0;
+}
+
+//-------------------------------
+//            OUTPUT
+//-------------------------------
+
+// -> Learning C
